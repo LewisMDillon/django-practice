f7b7943aebb0b224400bc8be35b8957ac2fd077c
LewisMDillon <lewismdillon1@gmail.com>
2023-07-03 11:22:45 +0000

    Migrate Item model and implement into site

--- todo/models.py
+++ todo/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-    name = models.CharField(max_length=50, null=false, blank=false)
-    done = models.BooleanField(null=false, blank=false, default_value=false)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
